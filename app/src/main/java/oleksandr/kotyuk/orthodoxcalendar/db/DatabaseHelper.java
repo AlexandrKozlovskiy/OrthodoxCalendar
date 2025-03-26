8804600ec83ad1d5bd15461810393fc29f896d7c
OleksandrKotyuk <oleksandr.kotyuk@gmail.com>
2025-03-26 22:25:02 +0200

    Добавлен календарь на второе полугодие 2025г.

--- app/src/main/java/oleksandr/kotyuk/orthodoxcalendar/db/DatabaseHelper.java
+++ app/src/main/java/oleksandr/kotyuk/orthodoxcalendar/db/DatabaseHelper.java
@@ -69,7 +69,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "calendar.db";
 
     // Номер версии этой БД
-    public static final int DATABASE_VERSION = 103;
+    public static final int DATABASE_VERSION = 104;
     public static String DBVersion = "db_version";
     private static DatabaseHelper dbHelper = null;
 
